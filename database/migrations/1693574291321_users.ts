08f5362f113836cf115e17fa0be9cf4b9051901d
Amorais19 <alicemoraisams@gmail.com>
2023-09-01 10:29:38 -0300

    mensagem

---
+++ database/migrations/1693574291321_users.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+
+      /**
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
+
+// migration: garante a melhora e o desenvolvimento do meu sistema
+// 2 pág: na de user - mantem os dados de autenticação do usuário; na de token - user online e expiração de tokens
